f4cd6a44aad39e40e5087e7fcf6f06d88d3558c2
keerunu <nacho_rodao13@hotmail.com>
2023-08-07 15:17:39 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,3 +19,10 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+async function getJson () {
+  const response = await fetch(DATA_URL);
+  const json = await response.json();
+  showData(json.students);
+}
+
+getJson();
